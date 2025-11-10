287c9a675a0329fd04e6d12dc783647ab7c1b441
kawsar-ahmmed9990 <kawsarahmmed9990@gmail.com>
2025-11-10 19:11:26 +0600

    add banner section

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <section className="relative h-[80vh] flex items-center justify-center bg-cover bg-center [background-image:url('https://i.ibb.co.com/Kcjkhh0t/photo-1533473359331-0135ef1b58bf.jpg')]">
+      <div className="absolute inset-0 bg-black/50"></div>
+
+      <div className="relative text-center text-white px-5">
+        <h1 className="text-4xl md:text-6xl font-bold mb-4">
+          Discover Your Perfect Ride
+        </h1>
+        <p className="text-lg md:text-xl mb-8">
+          Rent, explore, and travel with ease — all in one place.
+        </p>
+        <button className="bg-yellow-400 hover:bg-yellow-500 text-black font-semibold px-6 py-3 rounded-full shadow-md transition duration-300">
+          All Vehicles
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default Banner;
--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from "react-router";
 import RootLayout from "../Layouts/RootLayout";
+import Home from "../Pages/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout></RootLayout>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
   },
 ]);
 export default router;
